3f4321caea43a90144b4ac600a18ac337e7758bc
sungmin <lsmorigin@gmail.com>
2014-11-18 11:20:30 +0900

    로그인 폼
    간단한 로그인폼 구현 예제입니다

---
+++ 로그인 폼/로그인 폼/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace 로그인_폼
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다.
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마십시오.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblId = new System.Windows.Forms.Label();
+            this.lblPwd = new System.Windows.Forms.Label();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.txtId = new System.Windows.Forms.TextBox();
+            this.txtPwd = new System.Windows.Forms.TextBox();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblId
+            // 
+            this.lblId.AutoSize = true;
+            this.lblId.Location = new System.Drawing.Point(12, 38);
+            this.lblId.Name = "lblId";
+            this.lblId.Size = new System.Drawing.Size(53, 12);
+            this.lblId.TabIndex = 0;
+            this.lblId.Text = "아이디 : ";
+            // 
+            // lblPwd
+            // 
+            this.lblPwd.AutoSize = true;
+            this.lblPwd.Location = new System.Drawing.Point(12, 77);
+            this.lblPwd.Name = "lblPwd";
+            this.lblPwd.Size = new System.Drawing.Size(65, 12);
+            this.lblPwd.TabIndex = 1;
+            this.lblPwd.Text = "비밀번호 : ";
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Location = new System.Drawing.Point(27, 161);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(41, 12);
+            this.lblResult.TabIndex = 2;
+            this.lblResult.Text = "결과 : ";
+            // 
+            // txtId
+            // 
+            this.txtId.Location = new System.Drawing.Point(83, 29);
+            this.txtId.Name = "txtId";
+            this.txtId.Size = new System.Drawing.Size(153, 21);
+            this.txtId.TabIndex = 3;
+            // 
+            // txtPwd
+            // 
+            this.txtPwd.Location = new System.Drawing.Point(83, 74);
+            this.txtPwd.Name = "txtPwd";
+            this.txtPwd.Size = new System.Drawing.Size(153, 21);
+            this.txtPwd.TabIndex = 4;
+            this.txtPwd.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtPwd_KeyDown);
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Location = new System.Drawing.Point(12, 118);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(120, 25);
+            this.btnLogin.TabIndex = 5;
+            this.btnLogin.Text = "로그인";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+            // 
+            // btnClose
+            // 
+            this.btnClose.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.btnClose.Location = new System.Drawing.Point(152, 118);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(120, 25);
+            this.btnClose.TabIndex = 6;
+            this.btnClose.Text = "닫기";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 193);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.txtPwd);
+            this.Controls.Add(this.txtId);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.lblPwd);
+            this.Controls.Add(this.lblId);
+            this.Name = "Form1";
+            this.Text = "로그인";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblId;
+        private System.Windows.Forms.Label lblPwd;
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.TextBox txtId;
+        private System.Windows.Forms.TextBox txtPwd;
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.Button btnClose;
+    }
+}
+
---
+++ 로그인 폼/로그인 폼/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 로그인_폼
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            if (CheckData() == true)
+            {
+                this.lblResult.Text = "로그인 성공!";
+            }
+        }
+
+        private bool CheckData()
+        {
+            if (this.txtId.Text == "")
+            {
+                MessageBox.Show("로그인 아이디를 입력하세요.", "알림", MessageBoxButtons.OK
+                    , MessageBoxIcon.Warning);
+                this.txtId.Focus(); // 데이터 입력을 다시 요청하기 위하여 커서 포커스를 옮겨줌
+                return false;
+            }
+            else if (this.txtPwd.Text == "")
+            {
+                MessageBox.Show("로그인 아이디를 입력하세요.", "알림", MessageBoxButtons.OK
+                    , MessageBoxIcon.Warning);
+                this.txtPwd.Focus(); 
+                return false;
+            }
+            else
+            {
+                if (this.txtId.Text == "test" && this.txtPwd.Text == "1234")
+                {
+                    return true;
+                }
+                else
+                {
+                    this.lblResult.Text = "아이디 혹은 비밀번호가 틀렸습니다.";
+                    this.txtId.Focus();
+                    return false;
+                }
+            }
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        // txtPwd 컨트롤에 커서가 위치 하면서 
+        // 키보드 입력이 있을 때 발생하는 이벤트를 처리하는 핸들러
+        private void txtPwd_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Enter) // 키보드 입력값이 엔터일때 다음 행을 실행
+            {
+                if (CheckData() == true)
+                {
+                    this.lblResult.Text = "로그인 성공!";
+                }
+            }
+        }
+        
+    }
+}
